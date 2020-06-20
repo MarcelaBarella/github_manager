8c59e19decb4d66c742f52414264e06194990af9
Marcela Barella <marcela.barella@hotmail.com>
2020-06-20 13:36:28 -0300

    WIP: Fixes github integration

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import (Flask, request, g, session, redirect, url_for,
-                    render_template ,jsonify, render_template_string)
+                   render_template, jsonify, render_template_string,
+                   make_response)
 from flask_github import GitHub
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -7,11 +10,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 app = Flask(__name__)
-app.config['GITHUB_CLIENT_ID'] = 'banana'
-app.config['GITHUB_CLIENT_SECRET'] = 'banana'
-app.config['GITHUB_BASE_URL'] = 'https://api.github.com'
+app.config['GITHUB_CLIENT_ID'] = os.environ.get('GITHUB_CLIENT_ID')
+app.config['GITHUB_CLIENT_SECRET'] = os.environ.get('GITHUB_CLIENT_SECRET')
+app.config['GITHUB_BASE_URL'] = 'https://api.github.com/'
 app.config['GITHUB_AUTH_URL'] = 'https://github.com/login/oauth/'
-app.config['DATABASE_URI'] =  'sqlite:////tmp/github-flask.db'
+app.config['DATABASE_URI'] = 'sqlite:////tmp/github-flask.db'
 
 github = GitHub(app)
 
@@ -20,13 +23,14 @@ engine = create_engine(app.config['DATABASE_URI'])
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
-                                       
+
 Base = declarative_base()
 Base.query = db_session.query_property()
 
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -43,8 +47,11 @@ class User(Base):
 @app.before_request
 def before_request():
     g.user = None
-    if 'user_id' in session:
-        g.user = User.query.get(session['user_id'])
+    access_token = request.cookies.get('user_token')
+    if access_token:
+        import pdb
+        pdb.set_trace()
+        g.user = User.query.get(access_token)
 
 
 @app.after_request
@@ -56,8 +63,6 @@ def after_request(response):
 @app.route('/<provider>')
 @app.route('/index/<provider>')
 def test(provider):
-    import pdb
-    pdb.set_trace()
     if g.user:
         t = 'Hello! %s <a href="{{ url_for("user") }}">Get user</a> ' \
             '<a href="{{ url_for("repo") }}">Get repo</a> ' \
@@ -91,17 +96,17 @@ def get_token():
 
 @app.route('/github-callback')
 @github.authorized_handler
-def authorized(access_token):
+def authorized(oauth_token):
     next_url = request.args.get('next') or url_for('index')
-    if access_token is None:
+    if oauth_token is None:
         return redirect(next_url)
 
-    user = User.query.filter_by(access_token=access_token).first()
+    user = User.query.filter_by(access_token=oauth_token).first()
     if user is None:
-        user = User(access_token)
+        user = User(oauth_token)
         db_session.add(user)
 
-    user.access_token = access_token
+    user.access_token = oauth_token
 
     g.user = user
     github_user = github.get('/user')
@@ -110,8 +115,11 @@ def authorized(access_token):
 
     db_session.commit()
 
-    session['user_id'] = user.id
-    return redirect(next_url)
+    import pdb
+    pdb.set_trace()
+    resp = make_response(redirect('/'))
+    resp.set_cookie('user_token', oauth_token)
+    return resp
 
 
 @app.route('/login')
